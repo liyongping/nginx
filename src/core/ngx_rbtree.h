bbb8053818e78e564087d7e9e1107004147c7db1
liyongping <lypkiller@gmail.com>
2012-03-03 22:37:57 +0800

    test commit

--- src/core/ngx_rbtree.h
+++ src/core/ngx_rbtree.h
@@ -35,12 +35,12 @@ typedef void (*ngx_rbtree_insert_pt) (ngx_rbtree_node_t *root,
     ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel);
 
 struct ngx_rbtree_s {
-    ngx_rbtree_node_t     *root;
-    ngx_rbtree_node_t     *sentinel;
+    ngx_rbtree_node_t     *root;	//树根
+    ngx_rbtree_node_t     *sentinel;//哨兵nil
     ngx_rbtree_insert_pt   insert;
 };
 
-
+// 红黑树初始的根一开始设置为哨兵结点.
 #define ngx_rbtree_init(tree, s, i)                                           \
     ngx_rbtree_sentinel_init(s);                                              \
     (tree)->root = s;                                                         \
@@ -58,18 +58,18 @@ void ngx_rbtree_insert_timer_value(ngx_rbtree_node_t *root,
     ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel);
 
 
-#define ngx_rbt_red(node)               ((node)->color = 1)
-#define ngx_rbt_black(node)             ((node)->color = 0)
-#define ngx_rbt_is_red(node)            ((node)->color)
-#define ngx_rbt_is_black(node)          (!ngx_rbt_is_red(node))
+#define ngx_rbt_red(node)               ((node)->color = 1)		//染红色
+#define ngx_rbt_black(node)             ((node)->color = 0)		//染黑色
+#define ngx_rbt_is_red(node)            ((node)->color)			//判断是否是红色节点
+#define ngx_rbt_is_black(node)          (!ngx_rbt_is_red(node))	//判断是否是黑色节点
 #define ngx_rbt_copy_color(n1, n2)      (n1->color = n2->color)
 
 
 /* a sentinel must be black */
-
+// 哨兵结点必须为黑色
 #define ngx_rbtree_sentinel_init(node)  ngx_rbt_black(node)
 
-
+// 找到node最左下的node
 static ngx_inline ngx_rbtree_node_t *
 ngx_rbtree_min(ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel)
 {
