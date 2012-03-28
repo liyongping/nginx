d642043c616cbc18cca682b0f294ce4f4567ac8a
liyongping <lypkiller@gmail.com>
2012-03-28 22:24:48 +0800

    comment on shmem

--- src/os/win32/ngx_shmem.c
+++ src/os/win32/ngx_shmem.c
@@ -14,7 +14,7 @@ ngx_shm_alloc(ngx_shm_t *shm)
 {
     u_char    *name;
     uint64_t   size;
-
+    // 申请一块空间存放共享内存名称
     name = ngx_alloc(shm->name.len + 2 + sizeof(NGX_INT32_LEN), shm->log);
     if (name == NULL) {
         return NGX_ERROR;
@@ -25,7 +25,7 @@ ngx_shm_alloc(ngx_shm_t *shm)
     ngx_set_errno(0);
 
     size = shm->size;
-
+    // 创建一个新的文件映射内核对象
     shm->handle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE,
                                     (u_long) (size >> 32),
                                     (u_long) (size & 0xffffffff),
@@ -45,7 +45,7 @@ ngx_shm_alloc(ngx_shm_t *shm)
     if (ngx_errno == ERROR_ALREADY_EXISTS) {
         shm->exists = 1;
     }
-
+    // 将一个文件映射对象映射到当前应用程序的地址空间
     shm->addr = MapViewOfFile(shm->handle, FILE_MAP_WRITE, 0, 0, 0);
 
     if (shm->addr != NULL) {
@@ -55,7 +55,7 @@ ngx_shm_alloc(ngx_shm_t *shm)
     ngx_log_error(NGX_LOG_ALERT, shm->log, ngx_errno,
                   "MapViewOfFile(%uz) of file mapping \"%V\" failed",
                   shm->size, &shm->name);
-
+    //对象映射失败，关闭文件映射对象
     if (CloseHandle(shm->handle) == 0) {
         ngx_log_error(NGX_LOG_ALERT, shm->log, ngx_errno,
                       "CloseHandle() of file mapping \"%V\" failed",
@@ -69,12 +69,13 @@ ngx_shm_alloc(ngx_shm_t *shm)
 void
 ngx_shm_free(ngx_shm_t *shm)
 {
+    // 在当前应用程序的内存地址空间解除对一个文件映射对象的映射
     if (UnmapViewOfFile(shm->addr) == 0) {
         ngx_log_error(NGX_LOG_ALERT, shm->log, ngx_errno,
                       "UnmapViewOfFile(%p) of file mapping \"%V\" failed",
                       shm->addr, &shm->name);
     }
-
+    // 关闭文件映射对象
     if (CloseHandle(shm->handle) == 0) {
         ngx_log_error(NGX_LOG_ALERT, shm->log, ngx_errno,
                       "CloseHandle() of file mapping \"%V\" failed",
--- src/os/win32/ngx_shmem.h
+++ src/os/win32/ngx_shmem.h
@@ -14,16 +14,17 @@
 
 
 typedef struct {
-    u_char      *addr;
-    size_t       size;
-    ngx_str_t    name;
-    HANDLE       handle;
+    u_char      *addr;      //指向共享内存的指针
+    size_t       size;      //共享内存大小
+    ngx_str_t    name;      //共享内存名称
+    HANDLE       handle;    //共享内存句柄
     ngx_log_t   *log;
     ngx_uint_t   exists;   /* unsigned  exists:1;  */
 } ngx_shm_t;
 
-
+// 申请共享内存
 ngx_int_t ngx_shm_alloc(ngx_shm_t *shm);
+// 释放共享内存
 void ngx_shm_free(ngx_shm_t *shm);
 
 
